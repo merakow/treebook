48366f1d9c33054d03528b62240a75b5d1f71be8
Mark Rakow <awroooawroooawrooo@yahoo.com>
2014-09-24 21:01:15 -0700

    Added tests for custom routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,33 @@ module ApplicationHelper
 	# 	end
 	# end
 
+	# def bootstrap_class_for(flash_type)
+	#    case flash_type
+	#      when :success
+	#        "alert-success"
+	#      when :error
+	#        "alert-danger"
+	#      when :alert
+	#        "alert-warning"
+	#      when :notice
+	#        "alert-info"
+	#      else
+	#        flash_type.to_s
+	#    end
+	#  end
+
+  # def bootstrap_class_for flash_type
+  #   { success: "success", error: "danger", alert: "warning", notice: "info" }[flash_type] || flash_type.to_s
+  # end
+ 
+  # def flash_messages(opts = {})
+  #   flash.each do |flash_type, message|
+  #     concat(content_tag(:div, message, class: "alert alert-#{bootstrap_class_for(flash_type)} fade in") do 
+  #             concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+  #             concat message 
+  #           end)
+  #   end
+  #   nil
+  # end
+
 end
